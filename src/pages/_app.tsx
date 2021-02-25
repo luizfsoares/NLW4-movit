d160732a234f06fd15164ced172df8a74cf77ba5
Luiz Felipe Soares <38364154+luizfsoares@users.noreply.github.com>
2021-02-25 20:55:16 -0300

    add: Contexto - Componente Children

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import { ChallengesProvider } from '../contexts/ChallengeContexts'
 import '../styles/global.css' // Estilos globais também fica aqui, pois é algo que será aplicada a todas as páginas.
+
+
 
 function MyApp({ Component, pageProps }) {
   //Todos os componentes que forem estáticos, coloca aqui, pois será reaproveitado por todas as páginas.
-  return <Component {...pageProps} />
-}
+  return(
+
+      <ChallengesProvider>
+
+        <Component {...pageProps} />
+      </ChallengesProvider>
+
+)}
 
 export default MyApp
