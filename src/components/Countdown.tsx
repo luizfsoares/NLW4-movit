138efda7f834840870b6b809eb725cab0ce2d47d
Luiz Felipe Soares <38364154+luizfsoares@users.noreply.github.com>
2021-02-26 11:11:54 -0300

    udpt: transferindo funções para o novo contexto

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,12 +1,8 @@
 import styles from '../styles/components/Countdown.module.css'
 import {useState, useEffect, useContext} from 'react'
 import { ChallengesContext } from '../contexts/ChallengeContexts';
+import { CountdownContexts } from '../contexts/CountdownContexts';
 
-//useEffect é um hook do react que serve para dsparar efeitos colaterais, ou seja, quando algo mudar ou acontecer, eu quero executar alguma função.
-//Ele recebe dois parâmetros. O primeiro é o que quer executar (funcão). O segundo é quando eu quero executar a primeira função.
-
-
-let countdownTimeout: NodeJS.Timeout;
 
 
 
@@ -16,20 +12,7 @@ export function Countdown (){
     //Precisa adicionar um estado, visto que é um componente que vai mudar ao longo da aplicação
 
 
-    //ESTADOS
-    const [time, setTime] = useState(0.05 * 60); // retorna uma tupla, o valor e uma função para alterar o valor
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    //CONTEXTO
-    //Importando a função de começar uma novo desafio do contexto para esse componente
-    const { startNewChallenge } = useContext(ChallengesContext);
-
-
-
-    const minutes = Math.floor(time / 60); // Retorna o piso, pois pode dar um número quebrado
-    const seconds = time % 60; //Como o tempo já está em segundos, ao encontrar o valor exato de minutos, o resto representa os segundos atuais
-
+    const {startCountdown, minutes, seconds, hasFinished, isActive, endResetCountdown} = useContext(CountdownContexts);
 
     //Transforma em string e faz um split (como não foi passado nada no split, é caracter por caracter). Porém precisa ter no mínimo 2 caracteres para dar o split
     //Assim, se o minuto for abaixo de 10 (abaixo de 2 caracteres), por exemplo, a função padStart vai compltar com 0 a esquerda.
@@ -37,31 +20,7 @@ export function Countdown (){
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
 
-    function startCountdown(){
-        setIsActive(true);
-    }
 
-    function endResetCountdown(){
-        clearTimeout(countdownTimeout); //Limpa e para o delay da execução do useEffect()
-        setIsActive(false); //Para o Countdown pois ao entrar no useEffect, vai dar false a verificação
-
-        setTime(0.05 * 60);
-    }
-
-    useEffect(() =>{
-        
-        if (isActive && time > 0){
-            countdownTimeout = setTimeout(() =>{ //A função setTimeout() chama uma função ou avalia uma expressão a cada X milisegundos            
-                setTime(time - 1); //Sempre que o time muda aqui, vai executar o useEffect novamente.
-            }, 1000);
-        }
-
-        else if(isActive && time == 0){
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive, time]) // É possível adicionar dois parâmetros de mudança, ou seja, sempre que mudar para ativo e sempre que o tempo mudar, aplica a função.
 
 
     //Retorno HTML
