2e58e8739932a3fa2e39f661370633e582cc9d09
Luiz Felipe Soares <38364154+luizfsoares@users.noreply.github.com>
2021-02-26 11:10:55 -0300

    udpt: Introduzindo novo Contexto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import { ChallengesProvider } from '../contexts/ChallengeContexts'
+import { CountdownProvider } from '../contexts/CountdownContexts'
 import '../styles/global.css' // Estilos globais também fica aqui, pois é algo que será aplicada a todas as páginas.
 
 
 
 function MyApp({ Component, pageProps }) {
-  //Todos os componentes que forem estáticos, coloca aqui, pois será reaproveitado por todas as páginas.
+  //Todos os componentes que forem estáticos, coloca aqui, pois será reaproveitado por todas as páginas.]
+  //Como o contexto de Countdown DEPENDE do contexto de Challenges, então não pode botar ele em volta.
+  //O Contexto de Challenges precisa ficar em volta de tudo
   return(
 
       <ChallengesProvider>
+        <CountdownProvider>
 
-        <Component {...pageProps} />
+          <Component {...pageProps} />
+        </CountdownProvider>
       </ChallengesProvider>
 
 )}
