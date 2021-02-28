52027fcab870e4160d45d2fbca0fbafa32a0d09c
Luiz Felipe Soares <38364154+luizfsoares@users.noreply.github.com>
2021-02-28 19:06:07 -0300

    udpt: atualizando e adicionando Cookies para salvar informações

--- src/contexts/ChallengeContexts.tsx
+++ src/contexts/ChallengeContexts.tsx
@@ -1,12 +1,18 @@
 import next from 'next';
 import { createContext, useState, ReactNode, useEffect} from 'react';
 import challenges from '../../challenges.json';
+import Cookies from 'js-cookie';
+import { LevelUpModal } from '../components/LevelUpModal';
 
 interface Challenge{
     type: 'body' | 'eye';
     description:string;
     amount:number;
 }
+
+
+
+
 
 interface ChallengesContextData {
     level: number;
@@ -18,35 +24,51 @@ interface ChallengesContextData {
     resetChallenge: () => void;
     nextLevelExperience: number;
     completeChallenge: ()=> void;
+    closeLevelModal: () => void;
   }
 
 
 
 interface ChallengesProviderProps{ //Funciona da mesma forma de type, atribuindo um tipo. Children é pq no app.tsx é um Componente ChallengesProvider que envolve outro.
     children: ReactNode;
+    level: number,
+    currentExperience: number,
+    currentNumberChallenges:number
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData); //Atribuindo o tipo dos valores do contexto
 
 //Quando é exportado essa função toda, o retorno dela é a tag com o value já setado que possui os estados e funções associadas.
 //Ao ser usado a tag exportada lá no _app.tsx, que envolve a aplicação toda, compartilha pra todos os componentes.
-export function ChallengesProvider({ children }: ChallengesProviderProps){
+export function ChallengesProvider({ children, ...rest}: ChallengesProviderProps){ //Rest operator, todas as propriedades fora a chidren.
 
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [currentNumberChallenges, setCurrentNumberChallenges] = useState(0);
+    const [level, setLevel] = useState(rest.level ?? 1); //rest.level e se não existir, usa 1
+    const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+    const [currentNumberChallenges, setCurrentNumberChallenges] = useState(rest.currentNumberChallenges ?? 0);
     const [currentChallenge, setCurrentChallenge] = useState(null);
+    const [isLevelModalOpen, setIsLevelModalOpen] = useState(false);
 
 
     //Pedir permissões para mostrar notificiação;
     //Quando o segundo argumento é um [] vazio, vai executar uma unica vez quando o componente aparecer pela primeira vez na tela
     useEffect(() => {
-        Notification.requestPermission();
+        
     }, [])
+
+    //Executar a função sempre que o que tiver dentro do [] mudar
+    //Armazenar nos cookies
+    useEffect(() =>{
+        //Cookies só armazena texto, por isso precisa fazer a conversão pyaara string
+        Cookies.set('level', String(level));
+        Cookies.set('currentExperience', String(currentExperience));
+        Cookies.set('currentNumberChallenges', String(currentNumberChallenges));
+    }, [level, currentExperience, currentNumberChallenges])
 
     function levelUp(){
 
         setLevel(level + 1);
+        setIsLevelModalOpen(true);
+        
     }
 
     function startNewChallenge(){
@@ -69,6 +91,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps){
     function resetChallenge(){
 
         setCurrentChallenge(null);
+    }
+
+    function closeLevelModal(){
+
+        setIsLevelModalOpen(false);
     }
 
     //Calculo usado por games de RPG para calcular a experiência do proximo nível
@@ -102,9 +129,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps){
 
         <ChallengesContext.Provider value={{level, levelUp, currentExperience, currentNumberChallenges, 
                                             startNewChallenge, currentChallenge, resetChallenge, nextLevelExperience,
-                                            completeChallenge}}>
+                                            completeChallenge, closeLevelModal}}>
 
-            {children}
+            {children /*Posso botar meu modal aqui, pois ele pode ser usado em cantos que tenham o Challenges Provider e como é um componente, pela hierarquia pode ser botado aqui */}
+            
+            {isLevelModalOpen ? <LevelUpModal/> : null}
         </ChallengesContext.Provider>
     );
 }
